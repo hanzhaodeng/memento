930db374547515bf796b8ffe053f07e68f2097e6
hanzhaodeng <hanzhaodeng@gmail.com>
2021-03-16 02:20:33 -0700

    mori working

---
+++ src/components/Mori.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-container>
+    <v-row dense v-for="mr in 4" :key="'mr' + mr">
+      <v-col v-for="mc in 4" :key="'mc' + mc">
+        <v-row no-gutters v-for="r in 10" :key="'r' + r">
+          <v-col v-for="c in 26" :key="'c' + c">
+            <div
+              class="box"
+              :class="{ filled: isFilled({ mr, mc, r, c }) }"
+            ></div>
+          </v-col>
+        </v-row>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class HelloWorld extends Vue {
+  @Prop() private value!: number;
+
+  isFilled({
+    mc,
+    mr,
+    r,
+    c
+  }: {
+    mc: number;
+    mr: number;
+    r: number;
+    c: number;
+  }): boolean {
+    const index = c + (r - 1) * 26 + (mc - 1) * 260 + (mr - 1) * 1040;
+    return index < this.value;
+  }
+}
+</script>
+
+<style scoped>
+.box {
+  margin: 1px;
+  border-style: solid;
+  border-width: 1px;
+  border-color: black;
+  width: 7px;
+  height: 7px;
+}
+
+.box.filled {
+  background-color: black;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,16 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <Mori />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import Mori from "@/components/Mori.vue";
 
 @Component({
   components: {
-    HelloWorld
+    Mori
   }
 })
 export default class Home extends Vue {}
